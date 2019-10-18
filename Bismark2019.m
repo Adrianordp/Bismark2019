c294467d3cfbf77c82340be6e8d6907cc2ce586a
Adriano Debian <adrianordp@alu.ufc.br>
2019-10-18 16:33:30 -0300

    Added plotSimu stop callback function

--- Bismark2019.m
+++ Bismark2019.m
@@ -1,15 +1,16 @@
 reseta
 
+Tsim = 60;
+
 Ts = 0.1;
 s = tf('s');
 z = tf('z',Ts);
 P1s = (-s+1)/(2*s+1)/(3*s+1)*exp(-s);
-step(P1s)
+step(P1s,Tsim)
 
 P1z = c2d(P1s,Ts);
 d = P1z.InputDelay;
 
-K = [39.5284 40.4371];
 V = (5.155*z^3 - 9.848*z^2 + 4.704*z)/(z-0.9)^2/(z-0.74);
 
 [A,B,C,D]  = tf2ss(P1z.num{1},P1z.den{1});
