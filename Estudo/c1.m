0fcd48820949af8da7e101b4ff46f78f819a8907
Adriano Debian <adrianordp@alu.ufc.br>
2019-12-16 15:50:03 -0300

    Not fully working

--- Estudo/c1.m
+++ Estudo/c1.m
@@ -7,7 +7,7 @@ s = tf('s');
 % Simulation parameters
 Tsim       = 60   ; % Total time
 distTime   = 30   ; % Dirtubance time
-distAmp    =-1  ; % Dirtubance amplitude
+distAmp    =-1    ; % Dirtubance amplitude
 noiseTime  = 55   ; % Noise input time
 noisePower = 1e-5 ; % Noise input power
 refAmp     = 1    ; % Reference
@@ -37,12 +37,12 @@ Gss = ss(A,B,C,D,Ts);
 p      = roots(Pr.den{1}); % open-loop poles
 ze     = roots(Pr.num{1});
 r1     = .975; % .975
-r2     = .97;  % .97
-rho    = [r1 r2];
-alphaf = .95; % .95
+r2     = .97; % .97
+rho    = [r1; r2];
+alphaf = .0; % .95
 betaf  = .90; % .90
-betav  = .8; % .95
-betavs = [.95 .965]; % .965
+betav  = [.8 .8 .74]; % .95
+betavs = [.9 .9 .74]; % .965
 nz     = 2;
 nDv    = length(betav);
 nDvs   = length(betavs);
@@ -60,9 +60,16 @@ F  = minreal(Kr*(1-betaf)^2*z^2/(z-betaf)^2*(1-alphaf*z^-1)^nz/(1-alphaf)^nz);
 
 nG  = G1z.num{1} ;
 dG  = G1z.den{1} ;
-dV  = [1 -betav ];
-% dVs = [1 -betavs];
-dVs = conv([1 -betavs(1)],[1 -betavs(2)]);
+dV = 1;
+for i = 1:nDv
+    dV = conv(dV,[1 -betav(i)]);
+end
+
+dVs = 1;
+for i = 1:nDvs
+    dVs = conv(dVs,[1 -betavs(i)]);
+end
+% dVs = tf(dVs,1,Ts);
 
 Ng_p1 = polyval(nG,p(1));
 Ng_p2 = polyval(nG,p(2));
@@ -74,43 +81,57 @@ phi = 0;
 for i = 1:d
     phi = phi + K*A^(i-1)*B*z^-i;
 end
-phi = minreal(phi);
-% rho = [.91 .92];
-x  = [rho 1 1];
+phi  = minreal(phi);
+NN   = nNv+nNvs+2;
 Nrho = length(rho);
-N  = length(x);
-% N  = nv+nvs+2;
-Av = zeros(N);
-Bv  = zeros(N,1);
-for i = 1:N
-    Pis = 1
-    for j = 1:nDvs
-        Pis = Pis*(x(i)-betavs(j));
-    end
-    Pi = 1
-    for j = 1:nDv
-        Pi = Pi*(x(i)-betav(j));
-    end
+Nr   = NN-Nrho-2;
+% Nr   = 0;
+x    = [rho; rand(Nr,1)*.2+.8; 1; 1];
+Nx   = length(x);
+Av   = zeros(Nx,NN);
+Bv   = zeros(Nx,1);
+
+Pis = 1
+for j = 1:nDvs
+    Pis = conv(Pis,[1 -betavs(j)]);
+end
+Pis = tf(Pis,1,Ts);
+
+Pi = 1
+for j = 1:nDv
+    Pi = conv(Pi,[1 -betav(j)]);
+end
+Pi = tf(Pi,1,Ts);
+Gs = K*((z*eye(size(A))-A)\B);
+for i = 1:Nx
     if i <= Nrho
-%          Av(i,:) = [rho(i)^(-d+1)/(rho(i)-betavs) rho(i)^-d/(rho(i)-betavs) evalfr(-Pr,rho(i))*rho(i)/(rho(i)-betav) evalfr(-Pr,rho(i))/(rho(i)-betav)];
         for j = 1:nNvs+1
-            Av(i,j) = rho(i)^(nNvs-d-j+1)/Pis
+            Av(i,j) = x(i)^(nNvs-d-j+1)/evalfr(Pis,x(i));
         end
         for j = 1:nNv+1
-            Av(i,j+nNvs+1) = -evalfr(Pr,rho(i))*rho(i)^(nNv-j+1)/Pi
+            Av(i,j+nNvs+1) = -evalfr(Pr,x(i))*x(i)^(nNv-j+1)/evalfr(Pi,x(i));
         end
-        Bv(i) = evalfr(phi,rho(i))+1;
-    elseif i == Nrho+1
+        Bv(i) = evalfr(phi,x(i))+1;
+    elseif i <= Nrho+Nr
+        for j = 1:nNvs+1
+            Av(i,j) = x(i)^(nNvs+1-d-j);
+        end
+        Av(i,1:nNvs+1) = Av(i,1:nNvs+1)/evalfr(Pis,x(i));
+        for j = 1:nNv+1
+            Av(i,j+nNvs+1) = x(i)^(nNv-j+1);
+        end
+        Av(i,nNvs+2:end) = -evalfr(Pr,x(i))*Av(i,nNvs+2:end)/evalfr(Pi,x(i));
+        Bv(i) = evalfr(phi,x(i))-evalfr(Gs,x(i));
+    elseif i == Nrho+Nr+1
         Av(i,:) = [ones(1,nNvs+1) zeros(1,nNv+1)];
-        Bv(i) = (evalfr(phi,1)+1)*Pis;
+        Bv(i) = (evalfr(phi,1)+1)*evalfr(Pis,x(i));
     else
         Av(i,:) = [zeros(1,nNvs+1) ones(1,nNv+1)];
-        Bv(i) = Kr*Pi;
+        Bv(i) = Kr*evalfr(Pi,x(i));
     end
 end
 
 v = Av\Bv;
-% v = pinv(Av)*Bv;
 
 Vs = tf([v(1) v(2)],dVs,Ts)
 V  = tf([v(3) v(4)],dV,Ts)
@@ -134,7 +155,7 @@ end
 
 [zz,pp,kk] = zpkdata(YR,'v');
 for i = 1:Nrho
-    if (min(pp-rho(i)) < 1e-4)
+    if (abs(min(pp-rho(i))) < 1e-4)
         disp(['pole ' num2str(i) ' ok!'])
     else
         disp(['pole ' num2str(i) ' error!'])
